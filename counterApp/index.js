6481873a5aa9523470fa0e30544022b40f8fe5b4
herveth <29130889+herveth@users.noreply.github.com>
2025-03-03 08:24:17 -0500

    Added counter app files via upload

---
+++ counterApp/index.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "    // 1 -
+    saveEl.textContent += countStr   // 1 - 2 - 3 -
+   //countEl.textContent = 0
+    //count = 0
+    //console.log(saveEl)
+}
+
+
